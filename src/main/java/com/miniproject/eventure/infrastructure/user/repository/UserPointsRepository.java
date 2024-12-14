e7751be625674e8d44eb2bdcb37108f62cbae7c2
AnugrahLink26 <anugrah.link@gmail.com>
2024-12-14 23:14:26 +0700

    Create User

--- src/main/java/com/miniproject/eventure/infrastructure/user/repository/UserPointsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.miniproject.eventure.infrastructure.user.repository;
-
-import com.miniproject.eventure.entity.user.UserPoints;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPointsRepository extends JpaRepository<UserPoints, Long> {
-}
--- src/main/java/com/miniproject/eventure/usecase/user/CreateUserPointsUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.miniproject.eventure.usecase.user;
-
-import com.miniproject.eventure.entity.user.UserPoints;
-import com.miniproject.eventure.infrastructure.user.dto.CreateUserPointsRequestDTO;
-
-public interface CreateUserPointsUseCase {
-    UserPoints createUserPoints(CreateUserPointsRequestDTO req);
-}
